f418c8e76f4e9b7e51d20b03fd9741aba80fd0e9
Marcel Mravec <marcel.mravec@evolwings.net>
2025-10-19 22:47:17 +0200

    add ex02 solution

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,4 +1,5 @@
 #include "Account.hpp"
+#include <ctime>
 #include <iostream>
 
 int Account::_nbAccounts = 0;
@@ -6,64 +7,87 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-Account::Account(int initial_deposit) {
-    _accountIndex = _nbAccounts;
-    _amount = initial_deposit;
-    _nbDeposits = 0;
-    _nbWithdrawals = 0;
+Account::Account(int initial_deposit)
+    : _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0),
+      _nbWithdrawals(0) {
+
     _nbAccounts++;
     _totalAmount += _amount;
-    // Zde přijde váš kód pro inicializaci objektu...
-    // Budete muset pracovat se statickými i nestatickými proměnnými.
+    _displayTimestamp();
+    std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount
+              << ";created\n";
 }
 
-// Implementace destruktoru
 Account::~Account(void) {
-    // Zde přijde váš kód pro "úklid" při zničení objektu...
-    // Log soubor napoví, co se má vypsat.
+    _displayTimestamp();
+    std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount
+              << ";closed\n";
 }
 
 void Account::makeDeposit(int deposit) {
-    _amount += deposit;
+    int pAmount = this->_amount;
+
+    _displayTimestamp();
+
+    this->_amount += deposit;
+    this->_nbDeposits++;
     _totalNbDeposits++;
-    // Zde přijde váš kód pro zpracování vkladu...
+    _totalAmount += deposit;
+    std::cout << " index:" << _accountIndex << ":p_amount:" << pAmount
+              << ";deposit:" << deposit << ";amount:" << this->_amount
+              << ";deposits:" << _nbDeposits << '\n';
 }
 
 bool Account::makeWithdrawal(int withdrawal) {
-    _amount -= withdrawal;
-    _totalNbWithdrawals++;
-    //
-    return false;
+    _displayTimestamp();
+    std::cout << " index:" << _accountIndex << ":p_amount:" << _amount
+              << ";withdrawal:";
+    if (withdrawal <= _amount) {
+        this->_amount -= withdrawal;
+        this->_nbWithdrawals++;
+        _totalNbWithdrawals++;
+        _totalAmount -= withdrawal;
+        std::cout << ";amount:" << this->_amount
+                  << ";nb_withdrawals:" << this->_nbWithdrawals << '\n';
+        return true;
+    } else {
+        std::cout << "refused\n";
+        return false;
+    }
 }
 
-int Account::checkAmount(void) const {
-    //
-    return 0;
-}
+int Account::checkAmount(void) const { return this->_amount; }
 
 void Account::displayStatus(void) const {
-    // Zde přijde váš kód pro zobrazení stavu jednoho účtu...
+    _displayTimestamp();
+    std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount
+              << ";deposits:" << this->_nbDeposits
+              << ";withdrawals:" << this->_nbWithdrawals << '\n';
 }
 
-int Account::getNbAccounts(void) {
-    //
-    return 0;
-}
+int Account::getNbAccounts(void) { return _nbAccounts; }
 
-int Account::getTotalAmount(void) {
-    //
-    return 0;
-}
+int Account::getTotalAmount(void) { return _totalAmount; }
 
-int Account::getNbDeposits(void) {
-    //
-    return 0;
-}
-int Account::getNbWithdrawals(void) {
-    //
-    return 0;
-}
+int Account::getNbDeposits(void) { return _totalNbDeposits; }
+
+int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
 
 void Account::displayAccountsInfos(void) {
-    // Zde přijde váš kód pro zobrazení souhrnných informací...
+    _displayTimestamp();
+    std::cout << " accounts:" << getNbAccounts()
+              << ";total:" << getTotalAmount()
+              << ";deposits:" << getNbDeposits()
+              << ";withdrawals:" << getNbWithdrawals() << '\n';
 }
+
+void Account::_displayTimestamp(void) {
+    char buffer[20];
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+
+    strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S] ", ltm);
+    std::cout << buffer;
+}
+
+Account::Account(void) {}
